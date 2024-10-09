cf90314113addb4d173480292922696f2e855c66
Shivam Kumar Manjhi <113127627+Imshivam1@users.noreply.github.com>
2024-10-09 15:07:44 +0530

    Added assets and data

--- components/Grid.tsx
+++ components/Grid.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import { BentoGrid, BentoGridItem } from './ui/BentoGrid' // Importing components for the grid layout
 import { title } from 'process' // This might be unused, consider removing if unnecessary
+import { gridItems } from '@/data'
 
 const Grid = () => {
   return (
     <section id='about'> {/* Section for the grid, identified by 'about' */}
         <BentoGrid> {/* BentoGrid serves as the container for the grid items */}
             {/* Mapping over the array of items to dynamically create grid elements */}
-            {[{ title :'Title 1', description : 'Desc1', id: 1}].map((item) => (
+            {gridItems.map((item) => (
                 <BentoGridItem 
                 id={item.id} // Each BentoGridItem gets a unique ID
                 key={item.id} // React key to ensure uniqueness for each rendered item
