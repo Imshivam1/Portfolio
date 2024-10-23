9dd97411f9c81e090bc1138b9e95f8c1cbd2ca80
Shivam Kumar Manjhi <113127627+Imshivam1@users.noreply.github.com>
2024-10-23 13:34:34 +0530

    div size edited

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -17,7 +17,7 @@ const Hero = () => {
         </div>
 
         <div className='flex justify-center relative my-20 z-10'>
-            <div className='max-w-[80vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center'>
+            <div className='max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center'>
                 <h2 className='uppercase tracking-widest text-xs text-center text-blue-100 max-w-80'>
                     Crafting Dynamic Web Solutions with Code
                 </h2>
