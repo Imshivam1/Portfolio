601e659e45b6a4dd50b43897e451d216c503929e
Shivam Kumar Manjhi <113127627+Imshivam1@users.noreply.github.com>
2024-10-28 12:00:42 +0530

    added customs

--- components/Grid.tsx
+++ components/Grid.tsx
@@ -8,13 +8,14 @@ const Grid = () => {
     <section id='about'> {/* Section for the grid, identified by 'about' */}
         <BentoGrid> {/* BentoGrid serves as the container for the grid items */}
             {/* Mapping over the array of items to dynamically create grid elements */}
-            {gridItems.map((item) => (
+            {gridItems.map(({id, title, description, className, img, imgClassName, titleClassName, spareImg}) => (
                 <BentoGridItem 
-                id={item.id} // Each BentoGridItem gets a unique ID
-                key={item.id} // React key to ensure uniqueness for each rendered item
-                title={item.title} // Passing title as a prop to BentoGridItem
-                description={item.description} // Passing description as a prop to BentoGridItem
-                className={item.className}
+                id={id} // Each BentoGridItem gets a unique ID
+                key={id} // React key to ensure uniqueness for each rendered item
+                title={title} // Passing title as a prop to BentoGridItem
+                description={description} // Passing description as a prop to BentoGridItem
+                className={className}
+                img={img}
                 />
             ))}
         </BentoGrid>
--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -26,16 +26,22 @@ export const BentoGridItem = ({
   className,     // Optional className prop to style each item
   title,         // The title for the grid item
   description,   // Description for the grid item
-  header,        // Optional header element (can be an image, icon, etc.)
-  icon,          // Optional icon element to display in the grid item
   id,            // Required ID for each grid item, for uniqueness
+  img,
+  imgClassName,
+  titleClassName,
+  spareImg,
 }: {
   className?: string;  // className is optional and expects a string
   title?: string | React.ReactNode;  // Title can be a string or React node
   description?: string | React.ReactNode;  // Description can be a string or React node
   header?: React.ReactNode;  // Header can be any React node
   icon?: React.ReactNode;  // Icon can be any React node
-  id: number,  // ID is required and must be a number
+  id?: number;  // ID is required and must be a number
+  img?: string;
+  imgClassName?: string;
+  titleClassName?: string;
+  spareImg?: string;
 }) => {
   return (
     <div
@@ -44,10 +50,12 @@ export const BentoGridItem = ({
         "row-span-1 relative rounded-3xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4 dark:bg-black dark:border-white/[0.2] bg-white border border-transparent justify-between flex flex-col space-y-4",
         className // Merges any custom className passed via props
       )}
+      style={{
+        background: 'rgb(4,7,29)',
+        backgroundColor: 'gradient...'
+      }}
     >
-      {header} {/* Optional header (can be an image or a custom element) */}
       <div className="group-hover/bento:translate-x-2 transition duration-200">
-        {icon} {/* Optional icon displayed at the top */}
         <div className="font-sans font-bold text-neutral-600 dark:text-neutral-200 mb-2 mt-2">
           {title} {/* Title of the grid item */}
         </div>
