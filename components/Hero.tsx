f2827994d860b56c26e769a1e5e84d51ac4098d2
Shivam Kumar Manjhi <113127627+Imshivam1@users.noreply.github.com>
2024-11-25 20:23:44 +0530

    omit

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -19,7 +19,7 @@ const Hero = () => {
         <div className='flex justify-center relative my-20 z-10'>
             <div className='max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center'>
                 <h2 className='uppercase tracking-widest text-xs text-center text-blue-100 max-w-80'>
-                    Crafting Dynamic Web Solutions with Code
+                    Crafting Dynamic Web Solutions with the power of Coding
                 </h2>
                 <TextGenerateEffect 
                 className='text-center text-[40px] md:text-5xl lg:text-6xl'
