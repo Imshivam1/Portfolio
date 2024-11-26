74a57a64d4250042aa2a63bcd3e6f6232f257cec
Shivam Kumar Manjhi <113127627+Imshivam1@users.noreply.github.com>
2024-11-26 13:12:57 +0530

    Edited

--- components/ui/GridGlobe.tsx
+++ components/ui/GridGlobe.tsx
@@ -395,7 +395,7 @@ export function GlobeDemo() {
   ];
 
   return (
-    <div className="flex flex-row items-center justify-center py-20 h-screen md:h-auto dark:bg-black bg-white relative w-full">
+    <div className="flex items-center justify-center absolute -left-5 top-36 md:top-40 w-full h-full">
       <div className="max-w-7xl mx-auto w-full relative overflow-hidden h-full md:h-[40rem] px-4">
         <motion.div
           initial={{
