9e62afea59ec96d0f0cbd19b6969dc00dfd41be9
Shivam Kumar Manjhi <113127627+Imshivam1@users.noreply.github.com>
2024-11-27 20:02:11 +0530

    a

--- data/index.ts
+++ data/index.ts
@@ -38,7 +38,7 @@ export const navItems = [
     },
     {
       id: 4,
-      title: "Tech enthusiast with a passion for development.",
+      title: "Tech enthusiast with a passion for development and learning.",
       description: "",
       className: "lg:col-span-2 md:col-span-3 md:row-span-1",
       imgClassName: "",
