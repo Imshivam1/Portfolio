29970925d89cc2e90ee7e1e7da19299572ec4b06
Shivam Kumar Manjhi <113127627+Imshivam1@users.noreply.github.com>
2024-12-03 12:14:02 +0530

    added

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -105,6 +105,15 @@ export const BentoGridItem = ({
                   {item}
                 </span>
               ))}
+              <span className="py-4 px-3 rounded-lg text-center bg-[#10132e]" />
+              </div>
+              <div className="flex flex-col gap-3 lg:gap-8">
+              <span className="py-4 px-3 rounded-lg text-center bg-[#10132e]" />
+              {['Azure', 'Node.js', 'MongoDb'].map((item)=>(
+                <span key={item} className="py-2 lg:py-4 lg:px-3 px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-center bg-[#10132E]">
+                  {item}
+                </span>
+              ))}
               </div>
             </div>
         )}
--- data/index.ts
+++ data/index.ts
@@ -28,7 +28,7 @@ export const navItems = [
     },
     {
       id: 3,
-      title: "My tech stack & arsenal",
+      title: "My tech stack",
       description: "I am consistent in improving my skills.",
       className: "lg:col-span-2 md:col-span-3 md:row-span-2",
       imgClassName: "",
