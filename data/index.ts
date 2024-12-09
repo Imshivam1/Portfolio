986447247b9adeb58f20d27613eb3cb95a568562
Shivam Kumar Manjhi <113127627+Imshivam1@users.noreply.github.com>
2024-12-09 17:21:37 +0530

    added

--- data/index.ts
+++ data/index.ts
@@ -72,7 +72,7 @@ export const navItems = [
   export const projects = [
     {
       id: 1,
-      title: "3D Solar System Planets to Explore",
+      title: "3D Planets to Explore",
       des: "Explore the wonders of our solar system with this captivating 3D simulation of the planets using Three.js.",
       img: "/p1.svg",
       iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
