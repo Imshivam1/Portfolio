6ab0f636b8d18ed078cb27e3b9db2b1cd41a068b
Shivam Kumar Manjhi <113127627+Imshivam1@users.noreply.github.com>
2024-12-19 11:52:13 +0530

    a

--- data/index.ts
+++ data/index.ts
@@ -49,7 +49,7 @@ export const navItems = [
   
     {
       id: 5,
-      title: "Currently building a JS Animation library",
+      title: "Currently building a full fledged website and application.",
       description: "The Inside Scoop",
       className: "md:col-span-3 md:row-span-2",
       imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
